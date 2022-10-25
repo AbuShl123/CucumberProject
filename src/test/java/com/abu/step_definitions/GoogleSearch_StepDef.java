9c9312019feff3307190d783096754167e652841
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-10-26 01:20:51 +0500

    improving google search testing

--- src/test/java/com/abu/step_definitions/GoogleSearch_StepDef.java
+++ src/test/java/com/abu/step_definitions/GoogleSearch_StepDef.java
@@ -15,12 +15,24 @@ public class GoogleSearch_StepDef {
     public void user_is_on_google_search_page() {
         Driver.getDriver().navigate().to("https://www.google.com");
     }
+
     @When("user types apple in the google search box and clicks on enter")
     public void user_types_apple_in_the_google_search_box_and_clicks_on_enter() {
         googleSearchPage.searchBox.sendKeys("apple", Keys.ENTER);
     }
+
     @Then("user sees apple - Google Search is in the google title")
     public void user_sees_apple_google_search_is_in_the_google_title() {
-        assertEquals("apple - Поиск в Google", Driver.getDriver().getTitle(), "title verification is failed");
+        assertEquals("title verification is failed", "apple - Поиск в Google", Driver.getDriver().getTitle());
+    }
+
+    @When("user types {string} in the google search box and clicks on enter")
+    public void user_types_in_the_google_search_box_and_clicks_on_enter(String string) {
+        googleSearchPage.searchBox.sendKeys(string, Keys.ENTER);
+    }
+
+    @Then("user sees {string} is in the google title")
+    public void user_sees_is_in_the_google_title(String string) {
+        assertEquals("title verification is failed", string, Driver.getDriver().getTitle());
     }
 }
--- src/test/resources/Features/googleSearch.feature
+++ src/test/resources/Features/googleSearch.feature
@@ -2,8 +2,13 @@ Feature: Google Search Feature
   User story: As a user when i am on the google search page
   I should be able to search whatever i want and see any relevant information
 
-  @wip
   Scenario: Search Functionality result title verification
     Given user is on google search page
     When user types apple in the google search box and clicks on enter
     Then user sees apple - Google Search is in the google title
+
+  @wip
+  Scenario: Search Functionality result title verification
+    Given user is on google search page
+    When user types "apple" in the google search box and clicks on enter
+    Then user sees "apple - Поиск в Google" is in the google title
